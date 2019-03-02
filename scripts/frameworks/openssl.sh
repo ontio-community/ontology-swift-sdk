f58736c0faa8f1b3d26fddfab767f07f25a568e8
hsiaosiyuan0 <hsiaosiyuan0@outlook.com>
2019-03-02 09:13:44 +0800

    bugfix: fix building process - wait sub routines complete their works (#6)
    * crypto
    
    * auto build lib when xcodebuild
    
    * fix osx case-insensitive
    
    * sdk
    
    * code format
    
    * network
    
    * tx partial
    
    * tx done
    
    * tx tests
    
    * wait notification
    
    * oep4
    
    * oep4 test contract src
    
    * supports map
    
    * readme
    
    * make shared
    
    * 1. update readme
    2. iOS 10.0
    
    * armv7
    
    * gmp cc
    
    * bugfix: xcode inject env vars cause build failure
    
    * armv7
    
    * armv7
    
    * i386
    
    * exports fns
    
    * bugfix: modifier conflicts
    
    * expose init
    
    * expose init
    
    * expose init
    
    * update readme
    
    * add doc
    
    * add link
    
    * bugfix: wait sub build processes complete their works

--- scripts/frameworks/openssl.sh
+++ scripts/frameworks/openssl.sh
@@ -22,22 +22,8 @@ LOG=${BUILD_DIR}/openssl.log
 FMK_BASE_NAME=openssl
 FMK_FILE_NAME=openssl.framework
 FMK_FILE=${BUILD_DIR}/${FMK_FILE_NAME}
-FMK_DST=${DIR}/../../Frameworks/${FMK_FILE_NAME}
-
-# http://fitnr.com/showing-a-bash-spinner.html
-spinner() {
-  local pid=$!
-  local delay=0.35
-  local spinstr='|/-\'
-  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
-    local temp=${spinstr#?}
-    printf " [%c]  " "$spinstr"
-    local spinstr=$temp${spinstr%"$temp"}
-    sleep $delay
-    printf "\b\b\b\b\b\b"
-  done
-  printf "    \b\b\b\b"
-}
+FMK_DST_DIR=${DIR}/../../Frameworks
+FMK_DST=${FMK_DST_DIR}/${FMK_FILE_NAME}
 
 mkd() {
   dir=$1
@@ -101,7 +87,7 @@ build() {
   for arch in ${ARCHS}
   do
     echo "Building for: ${arch}"
-    (build_for $arch >> "${LOG}" 2>&1) & spinner
+    build_for $arch >> "${LOG}" 2>&1
   done
 }
 
@@ -193,7 +179,8 @@ create_framework() {
 
   create_modulemap
 
-  cp -rf $FMK_FILE ${DIR}/../../Frameworks/
+  mkdir -p $FMK_DST_DIR > /dev/null 2>&1
+  mv $FMK_FILE $FMK_DST_DIR/
 }
 
 skip_if_dup() {
